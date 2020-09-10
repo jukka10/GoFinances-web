9fd6f0277dcc68556d11c9ec5fd196a15bc725f8
jukka <jukka@iMac-de-jukka.home>
2020-09-10 10:03:08 -0300

    completed

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const formatedValue = Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
 
+  return formatedValue;
+};
 export default formatValue;
